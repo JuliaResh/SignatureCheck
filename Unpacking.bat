2af9363d1f809638d708fb48961fd8a30ef3138f
My username <lee.onore@gmail.com>
2015-11-09 21:52:31 +0300

    do not unpack jars

--- Unpacking.bat
+++ Unpacking.bat
@@ -3,7 +3,7 @@ REM this stuff should unpack everything except .exe files
 @FOR /D /r %%F in ("*") DO (
     @pushd %CD%
     @cd %%F
-       @FOR %%X in (*.rar *.zip *.jar *.war *.tar *.gz *.tar.gz) DO (
+       @FOR %%X in (*.rar *.zip *.war *.tar *.gz *.tar.gz) DO (
 	        %1\7z.exe x -y "%%X"
 	    )
     @popd
